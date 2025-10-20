e86f97574a9c78ca6f41065c6298b4657e5536da
Enzostik <enzoalmenar13@gmail.com>
2025-10-20 13:23:59 -0300

    Se añadió main.py
    Código principal que se ejecutará con una interfaz gráfica 'tkinter'.

---
+++ main.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+import calculator
+
+#Los cuerpos se pueden obtener del modulo 'calculator'
+bodies = calculator.BODIES_DATA
+
+#Configurar la ventana
+window = tk.Tk()
+window.title('Calculadora de volumen')
+
+#TODO: Insertar elementos de la ventana aquí
+
+window.mainloop()
