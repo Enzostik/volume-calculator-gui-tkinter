448affc774f71368f4aea85663d0f81556033e40
Enzostik <enzoalmenar13@gmail.com>
2025-10-24 13:09:30 -0300

    Estandarización de código
    Cambios al código siguiendo las recomendaciones del analizador Pylint.

--- calculator.py
+++ calculator.py
@@ -1,16 +1,33 @@
+'''
+Módulo encargado de definir las formas geométricas,
+determinando sus parámetros y calculando sus propiedades.
+'''
 from abc import ABC, abstractmethod
 from math import pi as PI
 
 #Clase base para todo cuerpo geométrico
 class Body(ABC):
+    '''
+    Clase base para definir los cuerpos geométricos.
+
+    Definir
+    ---
+        - volume : return `float`
+        - surface : return `float`
+    '''
     def __init__(self, name:str, **kwargs:dict[str, int|float]):
         self.name:str = name
         self.__parameters:dict[str, int|float] = kwargs
-    
     @property
     def parameters(self)->dict[str, int|float]:
+        '''
+        Obtiene una copia de los parámetros del cuerpo geométrico.
+
+        Para obtener/modificar un valor específico llama:
+        -   `Body.set(key, value)`:
+        -   `Body.get(key)`.
+        '''
         return self.__parameters.copy()
-    
     @parameters.setter
     def parameters(self, data:dict[str, int|float])->None:
         if not isinstance(data, dict):
@@ -18,57 +35,59 @@ class Body(ABC):
         if data.keys() == self.__parameters.keys():
             self.__parameters = data.copy()
         else:
-            raise Exception(f"Las llaves de 'data' deben coincidir con las de 'parameters': {list(self.__parameters.keys())}")
-
+            raise IndexError(f"Los índices no coinciden: {list(self.__parameters.keys())}")
     def set(self, key:str, value:int|float)->None:
+        '''
+        Cambia un valor de un parámetro ya definido del cuerpo geométrico.
+        
+        Si el parámetro no existe no hace nada.
+        '''
         if key in self.__parameters:
             self.__parameters[key] = value
-    
     def get(self, key:str)->int|float|None:
+        '''
+        Función para obtener los valores de los parámetros.
+
+        Si el parámetro no existe provoca un error.
+        '''
         try:
             return self.__parameters[key]
-        except:
-            return None
-
+        except KeyError as e:
+            raise KeyError('Parámetro no encontrado') from e
     @abstractmethod
     def volume(self)-> float:
         ''' Se debe declarar la fórmula de volumen '''
-        pass
-
+        return
     @abstractmethod
     def surface(self)-> float:
         ''' Se debe declarar la fórmula de superficie '''
-        pass
-
+        return
     def __str__(self):
         #Para obtener el nombre empezando con mayuscula y el resto en minúscula
         return self.name[0].upper()+self.name[1:].lower()
 
 #Clases para cada nuevo cuerpo geométrico
 class Sphere(Body):
+    '''
+    Cuerpo geométrico: Esfera
+    '''
     def __init__(self, radius:int|float = 0):
         super().__init__(name='Esfera', radio = radius)
-
     def volume(self)->float:
         return (4/3) * PI * self.get('radio')**3
-
     def surface(self)->float:
         return 4 * PI * self.get('radio')**2
 
 class Cube(Body):
+    '''
+    Cuerpo geométrico: Cubo
+    '''
     def __init__(self, side:int|float = 0):
         super().__init__(name='Cubo', lado=side)
-    
-    def volume(self):
+    def volume(self)->float:
         return self.get('lado')**3
-    
-    def surface(self):
+    def surface(self)->float:
         return 6 * (self.get('lado')**2)
 
-'''
--------------------------------------------
-    Creación de los cuerpos geométricos
--------------------------------------------
-'''
 #Variable donde guardar los cuerpos creados/configurados
 bodies_data:list[Body] = [Sphere(), Cube()]
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
+'''
+Implementación de interfáz gráfica tkinter para el cálculo de forma geométrica.
+'''
 import tkinter as tk
 from copy import deepcopy
 from PIL import Image, ImageTk
 
 import calculator
 
-'''
----------
-Variables
----------
-'''
 #Texto de saludo
 SALUDO = "Seleccione una figura del menú desplegable para calcular su volumen"
 
@@ -17,69 +15,91 @@ bodies = calculator.bodies_data
 
 #Elemento de ventana de tkinter
 window = tk.Tk()
+
+
 '''
 --------------------
 Funciones generales
 --------------------
 '''
-#Comprobar que el valor a ingresar sea un numero
 def validate_number(value:str)->bool:
+    '''
+    Verifica que el valor ingresado sea un número positivo.
+    '''
     if value != '': #Si no se borra el contenido
         try:
             return float(value) >= 0 #Tiene que ser un numero y debe ser mayor o igual a 0
-        except:
+        except ValueError:
             return False
     return True
 
-#Convertir una cadena de caractéres en su correspondiente número
 def string_to_float(value:str)->float:
+    '''
+    Convierte una cadena de caracteres a su correspondiente número.
+    '''
     return 0 if value == '' else float(value)
 
-#Convierte un número en una cadena de caractéres
 def format_value(value:float)->str:
+    '''
+    Da formato a un número para facilitar su lectura si es mayor o menor a 0.001
+    '''
     #Si el valor es menor a 0.001 utilizar exponente para poder leerlo
     if value == 0 or value > 0.001:
         return round(value, 4)
-    return "{:.4e}".format(value)
+    return f"{value:.4e}"
 
-'''
-------------------------
-Elementos de la ventana
-------------------------
-'''
+
+#Elementos de tkinter
+# Registrar la función para validar que la nueva entrada sea un número
+entry_verification = (window.register(validate_number), '%P')
+
 class CalculatorFrame:
+    '''
+    Cuadro principal donde colocar los elementos para la calculadora de cuerpos geométricos.
+    '''
     def __init__(self, root:tk.Tk):
-        #Variable que contenga el cuerpo para los calculos
+        # Variable que contenga el cuerpo para los calculos
         self.body = None
 
-        #Se crea un frame que ocupe toda la ventana. El contenido a editar se encuentra dentro de este frame.
-        self.main_frame = tk.Frame(root, borderwidth=1, relief="groove")
+        # Se crea un frame que ocupe toda la ventana.
+        # El contenido a editar se encuentra dentro de este frame.
+        self.main_frame = tk.Frame(root)
         self.main_frame.configure(bg="lightblue")
         self.main_frame.pack(expand=True, fill='both')
 
-        #Etiqueta de inicio
-        self.etiqueta_1 = tk.Label(self.main_frame, text=SALUDO, font=("Arial", 12), bg="gray",wraplength=350) #mensaje principal
+        # Etiqueta de inicio
+        self.etiqueta_1 = tk.Label(self.main_frame, text=SALUDO,
+                                   font=("Arial", 12), bg="gray",wraplength=350) #mensaje principal
         self.etiqueta_1.pack(side='top', fill='x')
 
-        #Registrar la function para validar que la nueva entrada sea un número
-        self.entry_verification = (root.register(validate_number), '%P')
-        #Almacenar las variables de las entradas en un diccionario para que no se borren al no ser referenciadas
+        # Almacenar las variables de las entradas en un diccionario
+        # para que no se borren al no ser referenciadas
         self.entries_var:dict[str, tk.StringVar] = {}
-        #Almacenar la referencia a la entrada que será destinada al resultado
+        # Almacenar la referencia a la entrada que será destinada al resultado
         self.var_volume = tk.StringVar()
-        self.result_volume:tk.Entry = tk.Entry(master=self.main_frame, textvariable=self.var_volume, font=("Arial", 15), fg="blue", justify='center', width=25, state='readonly')
+        self.result_volume:tk.Entry = tk.Entry(
+            master=self.main_frame, textvariable=self.var_volume, font=("Arial", 15),
+            fg="blue", justify='center', width=25, state='readonly'
+        )
         self.var_surface = tk.StringVar()
-        self.result_surface:tk.Entry = tk.Entry(master=self.main_frame, textvariable=self.var_surface, font=("Arial", 15), fg="blue", justify='center', width=25, state='readonly')
+        self.result_surface:tk.Entry = tk.Entry(
+            master=self.main_frame, textvariable=self.var_surface, font=("Arial", 15),
+            fg="blue", justify='center', width=25, state='readonly'
+        )
 
         #Click derecho para copiar los resultados
         self.popup_menu = tk.Menu(master=root, tearoff=0)
-        self.popup_menu.add_command(label='Copiar', command=lambda : [root.clipboard_clear(), root.clipboard_append(self.value_selected)])
-        self.result_volume.bind('<Button-3>', lambda event, result_var = self.var_volume : self.__show_popupmenu(result_var, event))
-        self.result_surface.bind('<Button-3>', lambda event, result_var = self.var_surface : self.__show_popupmenu(result_var, event))
+        self.popup_menu.add_command(
+            label='Copiar', command=lambda :
+            [root.clipboard_clear(), root.clipboard_append(self.value_selected)]
+        )
+        self.result_volume.bind('<Button-3>', lambda event, result_var = self.var_volume :
+                                self.__show_popupmenu(result_var, event))
+        self.result_surface.bind('<Button-3>', lambda event, result_var = self.var_surface :
+                                 self.__show_popupmenu(result_var, event))
         self.value_selected:float = None
-    
     #Variable que se llamará cuando se cambie alguna de las entradas de texto
-    def change_value(self, name:str, value:str):
+    def __change_value(self, name:str, value:str):
         #Si la entrada está vacía será 0. Convertir variable string --> float
         value = 0 if value == '' else float(value)
         #Cambiar el valor del parametro
@@ -89,51 +109,68 @@ class CalculatorFrame:
 
     #Cambiar el valor del texto de volumen y superficie
     def update_results(self):
+        '''
+        Actualiza los valores de los resultados del volumen y superficie.
+        Realizando sus respectivos cálculos nuevamente.
+        '''
         self.var_volume.set(format_value(self.body.volume()))
         self.var_surface.set(format_value(self.body.surface()))
 
     #Cambiar el contenido del frame para que corresponda al cuerpo geométrico
-    def load(self, body:calculator.Body):
+    def load(self, new_body:calculator.Body):
+        '''
+        Carga un cuerpo geométrico para representarlo en la interfáz gráfica.
+        '''
         #Eliminar el contenido del frame
         self.clean_frame()
         #Guardar el cuerpo
-        self.body = body
+        self.body = new_body
         #Modificar el título de la ventana para que sea el nombre del cuerpo
-        self.etiqueta_1.config(text=body)
+        self.etiqueta_1.config(text=new_body)
         #Cargar la imagen
         try: #Intenta agregar imagenes de la figura
-            img = Image.open(f'images/{body.name.lower()}.png') #Se carga el archivo con el mismo nombre de la forma en la carpeta imagenes
+            #Se carga el archivo con el mismo nombre de la forma en la carpeta imagenes
+            img = Image.open(f'images/{new_body.name.lower()}.png')
             img = img.resize((150, 150))
             image = ImageTk.PhotoImage(img) #Convertir la imagen para utilizarla en tkinter
             etiqueta_img = tk.Label(self.main_frame, image=image) #Añadir la imagen en un Label
             etiqueta_img.image = image
             etiqueta_img.pack(pady=5) #Añadir el label al frame
-        except: #Si no hay imagenes que coincidan con el nombre no hace nada
+        except FileNotFoundError: #Si no hay imagenes que coincidan con el nombre no hace nada
             pass
         #Etiqueta para las instrucciones
-        etiqueta_instruccion = tk.Label(self.main_frame, text="Ingrese los valores indicados:", font=("Arial", 10), justify='left', background='lightblue')
+        etiqueta_instruccion = tk.Label(self.main_frame, text="Ingrese los valores indicados:",
+                                        font=("Arial", 10), justify='left', background='lightblue')
         etiqueta_instruccion.pack(fill='x', pady=5) #Añadir la etiqueta al frame
 
         #Crear las entradas de texto para cada parámetro
-        
-        for key, value in body.parameters.items():
+        for key, value in new_body.parameters.items():
             #Etiqueta con el nombre de la variable
-            etiqueta_valor = tk.Label(self.main_frame, text=key[0].upper()+key[1:].lower(), font=("Arial", 10), background="gray")
+            etiqueta_valor = tk.Label(self.main_frame, text=key[0].upper()+key[1:].lower(),
+                                      font=("Arial", 10), background="gray")
             etiqueta_valor.pack(anchor='w', fill='x', pady=5)
             #Variable de la entrada de texto
             variable_valor = tk.StringVar(name=key, value=value)
             self.entries_var[key] = variable_valor
             #Entrada de texto
-            entrada_valor = tk.Entry(self.main_frame, textvariable=variable_valor, validate='key', validatecommand=self.entry_verification)
+            entrada_valor = tk.Entry(self.main_frame, textvariable=variable_valor,
+                                     validate='key', validatecommand=entry_verification)
             entrada_valor.pack(pady=5)
             #Evento en caso de modificación de la entrada de texto
-            variable_valor.trace_add('write', lambda name, _index, _mode, v = variable_valor : self.change_value(name, v.get()))
+            variable_valor.trace_add('write', lambda name, _index, _mode, v = variable_valor :
+                                     self.__change_value(name, v.get()))
 
-        #Añadir entradas de solo lectura para mostrar los resultados del volumen y de la superficie del cuerpo
-        etiqueta_volumen = tk.Label(self.main_frame, text=f"El volumen del {body.name.lower()} es:", font=("Arial", 15), fg="blue", bg='gray')
+        #Añadir entradas de solo lectura para mostrar los resultados
+        etiqueta_volumen = tk.Label(
+            self.main_frame, text=f"El volumen del {new_body.name.lower()} es:",
+            font=("Arial", 15), fg="blue", bg='gray'
+        )
         etiqueta_volumen.pack(fill='x', pady=5)
         self.result_volume.pack(pady=5)
-        etiqueta_superficie = tk.Label(self.main_frame, text=f"La superficie del {body.name.lower()} es:", font=("Arial", 15), fg="blue", bg='gray')
+        etiqueta_superficie = tk.Label(
+            self.main_frame, text=f"La superficie del {new_body.name.lower()} es:",
+            font=("Arial", 15), fg="blue", bg='gray'
+        )
         etiqueta_superficie.pack(fill='x', pady=5)
         self.result_surface.pack(pady=5)
         #Cambiar los valores de los resultados si es necesario
@@ -141,6 +178,9 @@ class CalculatorFrame:
 
     #Funcion para limpiar la pantalla cuando se cambia de opción de figura
     def clean_frame(self):
+        '''
+        Elimina/limpia todos los elementos agregados a la ventana.
+        '''
         self.body = None #Eliminar las variables previamente utilizadas
         self.entries_var.clear() #Eliminar las variables previamente utilizadas
         #Devolver la etiqueta principal al mensaje de entrada
@@ -160,24 +200,20 @@ class CalculatorFrame:
             self.value_selected = float(value_string.get())
             self.popup_menu.tk_popup(event.x_root, event.y_root, 0)
         finally:
-            self.popup_menu.grab_release() 
+            self.popup_menu.grab_release()
 
-'''
-----------------------------------
-Funciones para el manejo de entradas
-----------------------------------
-'''
-def select(body:calculator.Body):
-    main_frame.load(deepcopy(body))
+def select(new_body:calculator.Body):
+    '''
+    Seleccionar un cuerpo geométrico para ser representado en la interfaz de la ventana principal.
+    '''
+    main_frame.load(deepcopy(new_body))
 
 def clean_window():
+    '''
+    Borrar todo el contenido de la ventana principal y volver al mensaje por defecto.
+    '''
     main_frame.clean_frame()
 
-'''
----------------------
-Configurar la ventana
----------------------
-'''
 #Título de la ventana
 window.title('Calculadora de volumen')
 
